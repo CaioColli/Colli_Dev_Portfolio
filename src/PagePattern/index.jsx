961c43ede53faa7cf5462b2031f60190ca2a29a4
Caio Colli <caiocolli22@gmail.com>
2024-09-29 19:21:12 -0300

    Fix não exibir FloatingHeader tela projetos

--- src/PagePattern/index.jsx
+++ src/PagePattern/index.jsx
@@ -1,12 +1,18 @@
 import { ButtonToTop } from '@/ButtonToTop'
 import { FloatingHeader } from '@/FloatingHeader'
 import { PageContainer } from '@/PageContainer'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 export const PagePattern = () => {
+    const location = useLocation()
+
+    const hiddenInRoute = '/projetos'
+
     return (
         <PageContainer>
-            <FloatingHeader />
+            {location.pathname !== hiddenInRoute &&
+                <FloatingHeader />
+            }
             <Outlet />
             <ButtonToTop />
         </PageContainer>
